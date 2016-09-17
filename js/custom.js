af22289821b24fb2deb0c81af4058e94a778f915
joinluli <sayhello@revoluli.com>
2016-09-17 17:19:51 -0400

    Added on load on custom js

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,5 @@
 var homepage = $("body");
-TweenLite.to(homepage, 0.8, {opacity: "1",  paddingTop: "0px"});
+$(window).load(function(){
+	TweenLite.to(homepage, 0.8, {opacity: "1",  paddingTop: "0px"});
+});
+	
