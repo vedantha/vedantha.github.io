5cb2a1f77f85c03fac49af3bb4f18606a1c6c3d1
joinluli <sayhello@revoluli.com>
2016-09-24 16:27:20 -0400

    Some more additions and adjustments

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 var homepage = $("body");
 $(window).load(function(){
-	TweenLite.to(homepage, 0.8, {opacity: "1",  paddingTop: "0px"});
+	TweenLite.to(homepage, 0.8, {opacity: "1",  paddingTop: "0px", ease: Expo.easeOut, y: 0});
 });
 	
